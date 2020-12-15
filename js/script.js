be0aadc2ae9099b15d9c0f07396fc532a6f7c8ee
johannranudd <33978097+johannranudd@users.noreply.github.com>
2020-12-15 19:03:34 +0100

    slkdnf

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-const hamburger = document.querySelector('#hamburger');
-const menu = document.querySelector('.hidden-menu');
-const carouselInner = document.querySelector('.carousel-inner');
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector(".hidden-menu");
+const carouselInner = document.querySelector(".carousel-inner");
+const mainNav = document.querySelector(".main-nav");
+const nextBtn = document.querySelector(".carousel-control-next");
+const prevBtn = document.querySelector(".carousel-control-prev");
 
 
-hamburger.addEventListener('click', function () {
-    menu.classList.toggle('show-menu');
-    carouselInner.classList.toggle('show-carousel-inner')
+hamburger.addEventListener("click", function () {
+  menu.classList.toggle("show-menu");
+  carouselInner.classList.toggle("show-carousel-inner");
+  mainNav.classList.toggle("show-main-nav");
+//   nextBtn.classList.toggle('tranformX-100');
+//   prevBtn.classList.toggle('tranformX-100');
 
-})
+});
